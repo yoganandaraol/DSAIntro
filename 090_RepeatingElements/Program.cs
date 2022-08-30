e1190728eff249d1d78dc724960b9f26a5c6edae
Yogananda Rao Locherla <locherla@live.com>
2022-08-30 16:12:25 +0530

    Find Repeated Element in an array

---
+++ 090_RepeatingElements/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _090_RepeatingElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputA = new List<int> { 10, 5, 3, 4, 3, 5, 6 };
+            Console.WriteLine(FindRepeatedElement(inputA) == 5 ? "Passed" : "Failed");
+
+            inputA = new List<int> { 6, 10, 5, 4, 9, 120 };
+            Console.WriteLine(FindRepeatedElement(inputA) == -1 ? "Passed" : "Failed");
+        }
+
+        static int FindRepeatedElement(List<int> A)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (var item in A)
+            {
+                if (dic.ContainsKey(item))
+                    dic[item] += 1;
+                else
+                    dic[item] = 1;
+            }
+
+            foreach (var item in A)
+            {
+                if (dic[item] > 1) return item;
+            }
+
+            return -1;
+        }
+    }
+}
