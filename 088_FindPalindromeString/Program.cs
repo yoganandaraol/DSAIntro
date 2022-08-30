45ebc740835b6f919d1ff62bd5d98c81a70a129b
Yogananda Rao Locherla <locherla@live.com>
2022-08-31 01:23:54 +0530

    Longest palindromic string fix

--- 088_FindPalindromeString/Program.cs
+++ 088_FindPalindromeString/Program.cs
@@ -4,149 +4,102 @@ namespace _088_FindPalindromeString
 {
     class Program
     {
-        //TODO
         static void Main(string[] args)
         {
-            Console.WriteLine(LongestPalindromicSubstring("aarevveraa") == 10 ? "Passed" : "Failed");
-            Console.WriteLine(LongestPalindromicSubstring("aareveraaaa") == 9 ? "Passed" : "Failed");
-            Console.WriteLine(LongestPalindromicSubstring("aaaareveraa") == 9 ? "Passed" : "Failed");
-            //Console.WriteLine(LongestPalindromicSubstring("revveraa") == 6 ? "Passed" : "Failed");
-            //Console.WriteLine(LongestPalindromicSubstring("aarevver") == 6 ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstringLength("aarevveraa") == 10 ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstringLength("aareveraaaa") == 9 ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstringLength("aaaareveraa") == 9 ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstringLength("revveraa") == 6 ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstringLength("aarevver") == 6 ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstringLength("abb") == 2 ? "Passed" : "Failed");
+
+            Console.WriteLine(LongestPalindromicSubstring("aarevveraa") == "aarevveraa" ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstring("aareveraaaa") == "aareveraa" ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstring("aaaareveraa") == "aareveraa" ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstring("revveraa") == "revver" ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstring("aarevver") == "revver" ? "Passed" : "Failed");
+            Console.WriteLine(LongestPalindromicSubstring("abb") == "bb" ? "Passed" : "Failed");
         }
 
-        static int LongestPalindromeLength(string s, int lIndex, int rIndex)
+        static int LongestPalindromicSubstringLength(string A)
         {
-            int count = 0;
+            int maxLen = 0;
 
-            if (lIndex == rIndex)
+            for (int i = 0; i < A.Length; i++)
             {
-                for (int i = 0; i < s.Length / 2; i++)
-                {
-                    if (s[i - 1] == s[i + 1])
-                    {
-                        count++;
-                        continue;
-                    }
-                    break;
+                maxLen = Math.Max(maxLen, PalindromicSubstringLength(A, i, i));
+                if (i + 1 <= A.Length - 1)
+                    maxLen = Math.Max(maxLen, PalindromicSubstringLength(A, i, i + 1));
+            }
+           
+            return maxLen;
+        }
 
+        static string LongestPalindromicSubstring(string A)
+        {
+            int maxLen = 0;
+            string maxSubString = string.Empty;
+            for (int i = 0; i < A.Length; i++)
+            {
+                int len = PalindromicSubstringLength(A, i, i);
+                if (len > maxLen)
+                {
+                    maxLen = len;
+                    if (i == 0)
+                        maxSubString = A.Substring(0, maxLen);
+                    else
+                        maxSubString = A.Substring(i - maxLen / 2, maxLen);
+                }
+                if (i + 1 <= A.Length - 1)
+                {
+                    len = PalindromicSubstringLength(A, i, i + 1);
+                    if (len > maxLen)
+                    {
+                        maxLen = len;
+                        if (i == 0)
+                            maxSubString = A.Substring(0, maxLen);
+                        else
+                            maxSubString = A.Substring(i - ((maxLen / 2) - 1), maxLen);
+                    }
                 }
             }
 
-
-
-            return count;
+            return maxSubString;
         }
 
-        static int LongestPalindromicSubstring(string s)
+        private static int PalindromicSubstringLength(string s, int lIndex, int rIndex)
         {
-            int maxLen = 0;
-            int count = s.Length % 2 == 0 ? 0 : 1;
-            int sIndex = s.Length % 2 == 0 ? (s.Length - 1) / 2 : s.Length / 2;
-
-            Console.WriteLine(s[sIndex]);
-            int iteration;
-            if (s.Length % 2 > 0)
+            int len = 0;
+            if (lIndex == rIndex)
             {
-                for (int i = sIndex; i >= 0; i--)
+                len = 1;
+                if (lIndex == 0) return 1;
+                for (int i = 1; lIndex - i >= 0 && lIndex + i < s.Length; i++)
                 {
-                    iteration = 1;
-                    while (i - iteration >= 0)
-                    {
-                        if (s[i - iteration] == s[i + iteration])
-                        {
-                            count += 2;
-                            maxLen = Math.Max(maxLen, count);
-                        }
-                        else break;
-                        iteration++;
-                    }
+                    if (s[lIndex - i] == s[lIndex + i])
+                        len += 2;
+                    else break;
                 }
-
-                for (int i = sIndex + 1; i < s.Length; i++)
-                {
-                    iteration = 1;
-                    count = s.Length % 2 == 0 ? 0 : 1;
-                    while (i + iteration < s.Length)
-                    {
-                        if (s[i - iteration] == s[i + iteration])
-                        {
-                            count += 2;
-                            maxLen = Math.Max(maxLen, count);
-                        }
-                        else break;
-                        iteration++;
-                    }
-
-                }
-
             }
             else
             {
-                for (int i = sIndex; i >= 0; i--)
+                if (s[lIndex] == s[rIndex])
                 {
-                    iteration = 1;
-                    int lIndex = sIndex, rIndex = lIndex + 1;
-                    while (i - iteration >= 0)
+                    len = 2;
+                    if (lIndex == 0) return len;
+
+                    for (int i = 1; lIndex - i >= 0 && rIndex + i < s.Length; i++)
                     {
-                        Console.WriteLine($"\t{s[lIndex - iteration]}\t{s[rIndex + iteration]}");
-                        if (s[lIndex - iteration] == s[rIndex + iteration])
-                        {
-                            count += 2;
-                            maxLen = Math.Max(maxLen, count);
-                        }
+                        if (s[lIndex - i] == s[rIndex + i])
+                            len += 2;
                         else break;
-                        iteration++;
                     }
-                    sIndex--;
                 }
-
-                for (int i = sIndex + 1; i < s.Length-1; i++)
-                {
-                    iteration = 1;
-                    int lIndex = sIndex+1, rIndex = lIndex + 1;
-                    while (i - iteration >= 0)
-                    {
-                        Console.WriteLine($"\t{s[lIndex - iteration]}\t{s[rIndex + iteration]}");
-                        if (s[lIndex - iteration] == s[rIndex + iteration])
-                        {
-                            count += 2;
-                            maxLen = Math.Max(maxLen, count);
-                        }
-                        else break;
-                        iteration++;
-                    }
-
-
-                    count = s.Length % 2 == 0 ? 0 : 1;
-                    while (i + iteration < s.Length)
-                    {
-                        Console.WriteLine($"\t{s[i - iteration]}\t{s[i + iteration]}");
-                        if (s[i - iteration] == s[i + iteration])
-                        {
-                            count += 2;
-                            maxLen = Math.Max(maxLen, count);
-                        }
-                        else break;
-                        iteration++;
-                    }
-
-                }
-
-
-
-
-
-
-                
             }
 
-
-            Console.WriteLine("----------------------------------");
-
-
-            Console.WriteLine(maxLen);
-            return maxLen;
+            return len;
         }
+
     }
 }
 
