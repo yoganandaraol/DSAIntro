b845990479fc04218eced0fb327f85747506a086
Yogananda Rao Locherla <locherla@live.com>
2022-09-13 00:35:17 +0530

    Classes and Objects LinkedList basics  Notes

---
+++ 108_BinaryTreeTraversals/TraversalLoops.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _108_BinaryTreeTraversals
+{
+    public class TraversalLoops
+    {
+        public List<int> PreOrderTraversal(TreeNode root)
+        {
+            var res = new List<int>();
+            var temp = root;
+
+            if (temp != null)
+            {
+                res.Add(temp.val);
+                
+            }
+
+
+            return res;
+        }
+    }
+}
