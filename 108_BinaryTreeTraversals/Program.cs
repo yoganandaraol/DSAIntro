60b6adab24fd8b62ef5145085416a9013e6ec95f
Yogananda Rao Locherla <locherla@live.com>
2022-09-13 00:25:35 +0530

    Binary Tree Traversal Using Recursion

---
+++ 108_BinaryTreeTraversals/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _108_BinaryTreeTraversals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("------------ InOrder Traversal ------------");
+
+            var output = new List<int> {1, 3, 2 };
+            Console.WriteLine(Enumerable.SequenceEqual(TraversalRecursion.InOrderTraversal(Tree1()), output) ? "Passed" : "Failed");
+
+            output = new List<int> { 6, 1, 3, 2 };
+            Console.WriteLine(Enumerable.SequenceEqual(TraversalRecursion.InOrderTraversal(Tree2()), output) ? "Passed" : "Failed");
+
+            Console.WriteLine("------------ PreOrder Traversal ------------");
+
+            output = new List<int> { 1, 2, 3 };
+            Console.WriteLine(Enumerable.SequenceEqual(TraversalRecursion.PreOrderTraversal(Tree1()), output) ? "Passed" : "Failed");
+
+            output = new List<int> { 1, 6, 2, 3 };
+            Console.WriteLine(Enumerable.SequenceEqual(TraversalRecursion.PreOrderTraversal(Tree2()), output) ? "Passed" : "Failed");
+
+            Console.WriteLine("------------ PostOrder Traversal ------------");
+
+            output = new List<int> { 3, 2, 1 };
+            Console.WriteLine(Enumerable.SequenceEqual(TraversalRecursion.PostOrderTraversal(Tree1()), output) ? "Passed" : "Failed");
+
+            output = new List<int> { 6, 3, 2, 1 };
+            Console.WriteLine(Enumerable.SequenceEqual(TraversalRecursion.PostOrderTraversal(Tree2()), output) ? "Passed" : "Failed");
+
+        }
+
+        /*
+         * 
+               1
+                \
+                 2
+                /
+               3
+
+         */
+        static TreeNode Tree1()
+        {
+            return new TreeNode(
+                                    1,
+                                    null,
+                                    new TreeNode(
+                                                    2,
+                                                    new TreeNode(
+                                                                    3,
+                                                                    null,
+                                                                    null),
+                                                    null)
+                );
+        }
+
+        /*
+         * 
+                   1
+                  / \
+                 6   2
+                    /
+                   3
+
+         */
+        static TreeNode Tree2()
+        {
+            return new TreeNode(
+                                    1,
+                                    new TreeNode(
+                                                    6,
+                                                    null,
+                                                    null),
+                                    new TreeNode(
+                                                    2,
+                                                    new TreeNode(
+                                                                    3,
+                                                                    null,
+                                                                    null),
+                                                    null)
+                );
+        }
+
+    }
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { this.val = x; this.left = this.right = null; }
+        public TreeNode(int x, TreeNode left, TreeNode right) { this.val = x; this.left = left; this.right = right; }
+    }
+}
---
+++ 108_BinaryTreeTraversals/TraversalRecursion.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _108_BinaryTreeTraversals
+{
+    public class TraversalRecursion
+    {
+        public static List<int> InOrderTraversal(TreeNode root)
+        {
+            var ans = new List<int>();
+            InOrder(root, ans);
+            return ans;
+        }
+
+        static void InOrder(TreeNode root, List<int> nodes)
+        {
+            var temp = root;
+            if (temp == null)
+            {
+                return;
+            }
+
+            InOrder(temp.left, nodes);
+            nodes.Add(temp.val);
+            InOrder(temp.right, nodes);
+        }
+
+
+        public static List<int> PreOrderTraversal(TreeNode root)
+        {
+            var ans = new List<int>();
+            PreOrder(root, ans);
+            return ans;
+        }
+
+        static void PreOrder(TreeNode root, List<int> nodes)
+        {
+            var temp = root;
+            if (temp == null)
+            {
+                return;
+            }
+
+            nodes.Add(temp.val);
+            PreOrder(temp.left, nodes);
+            PreOrder(temp.right, nodes);
+        }
+
+        public static List<int> PostOrderTraversal(TreeNode root)
+        {
+            var ans = new List<int>();
+            PostOrder(root, ans);
+            return ans;
+        }
+
+        static void PostOrder(TreeNode root, List<int> nodes)
+        {
+            var temp = root;
+            if (temp == null)
+            {
+                return;
+            }
+
+            PostOrder(temp.left, nodes);
+            PostOrder(temp.right, nodes);
+            nodes.Add(temp.val);
+        }
+    }
+}
