abc935be6579bf0898e5e7daa67b587cd4276313
Yogananda Rao Locherla <locherla@live.com>
2022-09-17 13:56:49 +0530

    Stack - Check for valid order of paranthesis

---
+++ 109_StackParanthesisOrder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _109_StackParanthesisOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsParanthesisOrderValid("{([])}") == 1 ? "Passed" : "Failed");
+            Console.WriteLine(IsParanthesisOrderValid("(){") == 0 ? "Passed" : "Failed");
+            Console.WriteLine(IsParanthesisOrderValid("()[]") == 1 ? "Passed" : "Failed"); 
+            Console.WriteLine(IsParanthesisOrderValid("))))))))") == 0 ? "Passed" : "Failed"); 
+        }
+
+        static int IsParanthesisOrderValid(string A)
+        {
+            Stack<char> s = new Stack<char>();
+            int openCount = 0;
+            foreach (var item in A)
+            {
+                if (item == '{' || item == '[' || item == '(')
+                {
+                    s.Push(item);
+                    openCount++;
+                }
+                else if (item == '}' && s.Count > 0 && Convert.ToChar(s.Pop()) != '{')
+                    return 0;
+                else if (item == ']' && s.Count > 0 && Convert.ToChar(s.Pop()) != '[')
+                    return 0;
+                else if (item == ')' && s.Count > 0 && Convert.ToChar(s.Pop()) != '(')
+                    return 0;
+            }
+
+            return openCount > 0 && s.Count == 0 ? 1 : 0;
+        }
+    }
+}
---
+++ 110_StudentMarks/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _110_StudentMarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputA = new List<string> { "xsdf85", "hsdf85", "a3", "sb34", "c100" };
+            var output = solve(inputA);
+
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static List<string> solve(List<string> A)
+        {
+            Dictionary<string, StudentCard> res = new Dictionary<string, StudentCard>();
+            List<string> output = new List<string>();
+            foreach (var item in A)
+            {
+                int temp = 0;
+                int marks = 0;
+                string name = "";
+
+                for (int i = 1; i < item.Length; i++)
+                {
+                    if (int.TryParse(item.Substring(item.Length - i, i), out temp))
+                    {
+                        name = item.Substring(0, item.Length - i);
+                        marks = temp;
+                    }
+                }
+
+                res.Add(item, new StudentCard(name, marks));
+            }
+
+            var result = res.OrderByDescending(x => x.Value.marks).ThenBy(x => x.Value.name);
+            foreach (var item in result)
+            {
+                output.Add(item.Key);
+            }
+
+            return output;
+        }
+
+        class StudentCard
+        {
+            public string name;
+            public int marks;
+            public StudentCard(string name, int marks)
+            {
+                this.name = name;
+                this.marks = marks;
+            }
+        }
+    }
+}
